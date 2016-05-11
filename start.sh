2613448ceb17aa5e5cda44d98ba8bc89ed949cd5
Tsung-en Hsiao <joshua5201.eecs03@nctu.edu.tw>
2016-05-11 23:18:04 +0800

    Remove run.pid, systemd have been handling this

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-java -Xmx2048M -Xms2048M -jar minecraft_server.jar nogui &>> `date --rfc-3339=date`.log & 
-echo $! > run.pid
+java -Xmx2048M -Xms2048M -jar minecraft_server.jar nogui &>> `date --rfc-3339=date`.log &
